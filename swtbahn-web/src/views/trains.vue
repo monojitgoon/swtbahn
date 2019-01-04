3a4634158ebb5835e9dd8ec601729f76082f8741
Monojit Goon <monojit.goon@stud.uni-bamberg.de>
2019-01-04 02:15:29 +0100

    axios test

--- swtbahn-web/src/views/trains.vue
+++ swtbahn-web/src/views/trains.vue
@@ -1,15 +1,53 @@
 <template>
-  <div class="animated fadeIn">
-    <div class="row">trains</div>
+  <div class="hello">
+    <input type="button" @click="createPost()" value="submit">
   </div>
 </template>
 
+
+
 <script>
+import axios from "axios";
+
 export default {
   name: "trains",
-  components: {}
+
+  data() {
+    return {
+      postBody: null,
+
+      postTitle: null
+    };
+  },
+
+  created() {},
+
+  methods: {
+    createPost() {
+      var config = {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      };
+
+      let formData = new FormData();
+
+      formData.append("train", "train1");
+
+      axios
+        .post(
+          "http://localhost:8080/driver/grab-train",
+
+          formData, // the data to post
+
+          config
+        )
+        .then(response => {})
+
+        .catch(e => {
+          console.error(e);
+        });
+    }
+  }
 };
 </script>
-
-<style>
-</style>
