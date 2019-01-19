61f3e040347bec9ad79e59822cb04cda5d87fabe
Monojit Goon <monojit.goon@stud.uni-bamberg.de>
2019-01-19 16:21:56 +0100

    Omitting unnecessaries

--- swtbahn-web/src/App.vue
+++ swtbahn-web/src/App.vue
@@ -48,9 +48,6 @@ export default {
 <style lang="scss">
 @import url("./assets/css/font-awesome.min.css");
 @import url("./assets/css/themify-icons.css");
-@import url("./assets/css/pe-icon-7-filled.css");
-@import url("./assets/css/flag-icon.min.css");
-@import url("./assets/css/cs-skin-elastic.css");
 @import "./assets/scss/style";
 
 button {
