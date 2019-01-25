f933f7663d2a09b5ea2653b2a8dd4361e407d9c8
Monojit Goon <z003xzzm@MD1RGD8C>
2019-01-25 19:14:16 +0100

    Svg init

--- swtbahn-web/src/views/trackpanel.vue
+++ swtbahn-web/src/views/trackpanel.vue
@@ -2,10 +2,117 @@
   <div class="col-xs-12 col-md-12">
     <card header-text="Trackboard">
       <div class="card-body card-block">
-        <svg width="32" height="32">
+        <svg viewBox="0 0 700 300" style="border: black solid; border-style: dotted">>
           <g>
+            <!-- vertical -->
+            <path stroke="green" stroke-width=".2" d="M10 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M10 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M20 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M30 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M40 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M50 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M60 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M70 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M80 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M90 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M100 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M110 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M120 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M130 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M140 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M150 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M150 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M160 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M170 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M180 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M190 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M200 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M210 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M220 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M230 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M240 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M250 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M260 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M270 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M280 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M290 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M300 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M310 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M320 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M330 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M340 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M350 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M360 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M370 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M380 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M390 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M400 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M410 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M420 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M430 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M440 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M450 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M460 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M470 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M480 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M490 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M500 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M510 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M520 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M530 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M540 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M550 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M560 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M570 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M580 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M590 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M600 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M610 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M620 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M630 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M640 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M650 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M660 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M670 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M680 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M690 0 V300"></path>
+            <path stroke="green" stroke-width=".2" d="M700 0 V300"></path>
+
+            <!-- horizontal -->
+            <path stroke="green" stroke-width=".2" d="M0 10 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 20 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 30 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 40 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 50 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 60 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 70 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 80 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 90 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 100 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 110 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 120 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 130 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 140 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 150 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 160 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 170 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 180 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 190 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 200 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 210 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 220 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 230 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 240 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 250 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 260 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 270 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 280 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 290 H700"></path>
+            <path stroke="green" stroke-width=".2" d="M0 300 H700"></path>
+
             <!-- Track -->
-            <path stroke="rgb(0,0,0)" fill="rgb(0,0,0)" d="M 0,12 L 19,99 L 11,99 L 0,20 z "></path>
+            <path stroke="black" stroke-width="1" fill="none" d="M250 20 H100 V0"></path>
+            <path stroke="black" stroke-width="1" fill="none" d="M250 20 H100 V0 L60 20"></path>
           </g>
         </svg>
       </div>
